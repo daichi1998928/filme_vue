41698b804d1f80b39b428d7c1adcdde5a397344c
関口大地 <>
2019-05-12 15:05:49 +0900

    [finish]create_cd_success_message

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,11 @@
 class Track < ApplicationRecord
 
   belongs_to :product
+
+
+  validates :track_name, presence: true
+  validates :artist_name, presence: true
+  validates :track_order, presence: true
+
+
 end
