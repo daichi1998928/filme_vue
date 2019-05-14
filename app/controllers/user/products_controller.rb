31335e5afb47798a372debf8ae46ef498d8b13ef
関口大地 <>
2019-05-14 14:09:49 +0900

    [finish]product_search

--- app/controllers/user/products_controller.rb
+++ app/controllers/user/products_controller.rb
@@ -1,2 +1,11 @@
 class User::ProductsController < ApplicationController
+
+  def search
+    @search_products = Product.search(params[:product_title])
+    @search_count = @search_products.count
+  end
+
+  def index
+
+  end
 end
