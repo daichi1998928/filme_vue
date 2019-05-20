28bab32cbfdbcc20c5fc806baaecac1e3dbf2a39
関口大地 <>
2019-05-20 17:19:14 +0900

    [fix]

--- app/controllers/user/products_controller.rb
+++ app/controllers/user/products_controller.rb
@@ -7,18 +7,19 @@ class User::ProductsController < ApplicationController
 
   def index
     @products=Product.all
-    
+
   end
 
   def show
     @product=Product.find(params[:id])
     @traks1=Track.where(product_id:@product.id).where(disk_num:1).order(:track_order)
     @traks2=Track.where(product_id:@product.id).where(disk_num:2).order(:track_order)
-    @related_product=Product.where("product_title LIKE ?","%#{@product.product_title}%")
-                      .where.not(product_title: "#{@product.product_title}")
+    @related_product=Product.where("product_title LIKE ?","%#{@product.product_title}%").where.not(product_title: "#{@product.product_title}")
+    @new_product_review = ProductReview.new
+
+
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+
+
 end
