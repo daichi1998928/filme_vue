1b9562df0914690b52f3d50efd8dd427af792e3b
関口大地 <>
2019-05-28 17:52:11 +0900

    [finish]movie_review

--- app/controllers/user/movies_controller.rb
+++ app/controllers/user/movies_controller.rb
@@ -2,20 +2,26 @@ class User::MoviesController < ApplicationController
 
   def show
 
-    @movie = Movie.find(params[:id])
-    @movie_review = MovieReview.new
-    @movie_relation_product = Product.where("product_title LIKE ?", "%#{@movie.title}%").first
+    @movie = Movie.find_by(id: params[:id])
 
-    if @movie.movie_reviews.exists?
-      @movie_review_average = @movie.movie_reviews.average(:rate).ceil.to_s
-      @movie_review_average_file = 'star' + @movie_review_average + '.png'
+    if @movie != nil
+      @movie_review = MovieReview.new
+      @movie_relation_product = Product.where("product_title LIKE ?", "%#{@movie.title}%").first
+
+      if @movie.movie_reviews.exists?
+        @movie_review_average = @movie.movie_reviews.average(:rate).ceil.to_s
+        @movie_review_average_file = 'star' + @movie_review_average + '.png'
+      end
+    else
+      redirect_to  user_products_path
     end
-
-
   end
+
   def edit
   	@movie = Movie.find(params[:id])
   end
+
+
 
 private
   def movies_params
