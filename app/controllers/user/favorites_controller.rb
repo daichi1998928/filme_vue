b8b5b28c4571f6287a00633c83de674176047ce8
関口大地 <>
2019-06-08 21:14:10 +0900

    [finish]peging

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -6,7 +6,7 @@ class User::FavoritesController < ApplicationController
  end
 
  def index
-  @favorites=current_user.favorites.order(:created_at).page(params[:page]).per(8)
+  @favorites = current_user.favorites.order(:created_at).page(params[:page]).per(16)
  end
 
  def create
@@ -15,7 +15,7 @@ class User::FavoritesController < ApplicationController
     favorite.save
     redirect_to user_product_path(product.id)
  end
- 
+
  def destroy
     product=Product.find(params[:product_id])
     favorite = current_user.favorites.find_by(product_id: product.id)
