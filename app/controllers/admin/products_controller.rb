0598bb5a72568886fd0a76bc7a60b93d57238b79
kayo289 <kayokapi@icloud.com>
2019-06-09 02:46:49 +0900

    show check

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -47,11 +47,15 @@ class Admin::ProductsController < ApplicationController
   def change_status
     @history_item=History.find(:history_item)
     @history_item.update(product_status:ingredients.length)
+    redirect_to edit_admin_user_path(@history_item.id)
   end
 
   private
   def products_params
     params.require(:product).permit(:image, :product_title,:price,:label_name,:stock ,tracks_attributes: [:id,:product_id ,:track_name, :artist_name,:disk_num,:track_order, :_destroy])
   end
-
+  
+  def status_params
+    params.require(:history_item).permit(:checkbox,ingredients:[])
+  end
 end
