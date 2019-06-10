9209338c8d12fb4fbaf5dff9527bebe6604d8f22
kayo289 <kayokapi@icloud.com>
2019-06-11 00:38:54 +0900

    [fix]論理削除error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_paranoid
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
 
   has_many :product_reviews
   has_many :movie_reviews
