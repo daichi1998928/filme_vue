a3f9c537a4d643ec31801b487027f7b84e416c6a
OKURA Masafumi <masafumi.o1988@gmail.com>
2019-08-28 16:26:17 +0900

    before_actionで@historyをセット

--- app/controllers/user/histories_controller.rb
+++ app/controllers/user/histories_controller.rb
@@ -2,6 +2,7 @@ class User::HistoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :can_buy, only: [:new]
   before_action :address_entry?,only: [:create,:cash_deliver]
+  before_action :set_history, only: [:create, :cash_deliver]
 
   include User::HistoriesHelper
 
@@ -26,12 +27,11 @@ class User::HistoriesController < ApplicationController
     end
 
     def cash_deliver
-      history=History.find_by(juge_use: true, user_id:current_user)
-      history.pay_method=2
-      history.save
+      @history.pay_method=2
+      @history.save
 
       current_cart.cart_items.each do |cart_item|
-        history.history_items.create(product_id: cart_item.product_id,
+        @history.history_items.create(product_id: cart_item.product_id,
                                       product_price: Product.find(cart_item.product_id).price,
                                       quantity: cart_item.quantity,
                                       user_id:current_user.id
@@ -41,15 +41,14 @@ class User::HistoriesController < ApplicationController
       end
 
       current_cart.update(juge_use:false)
-      history.update(juge_use:false)
+      @history.update(juge_use:false)
       @cart=Cart.create(user_id:current_user.id)
       redirect_to products_buy_path, notice: "支払いが完了しました"
     end
 
     def create
-      history=History.find_by(juge_use: true, user_id:current_user)
-      history.pay_method=1
-      history.save
+      @history.pay_method=1
+      @history.save
       Payjp::Charge.create(
         amount: sum, # 決済する値段
         card: params['payjp-token'],
@@ -57,7 +56,7 @@ class User::HistoriesController < ApplicationController
       )
 
       current_cart.cart_items.each do |cart_item|
-        history.history_items.create(product_id: cart_item.product_id,
+        @history.history_items.create(product_id: cart_item.product_id,
                                      product_price: Product.find(cart_item.product_id).price,
                                      quantity: cart_item.quantity,
                                      user_id:current_user.id
@@ -66,12 +65,16 @@ class User::HistoriesController < ApplicationController
         @product.update(stock:@product.stock-cart_item.quantity)
       end
       current_cart.update(juge_use:false)
-      history.update(juge_use:false)
+      @history.update(juge_use:false)
       @cart=Cart.create(user_id:current_user.id)
       redirect_to products_buy_path, notice: "支払いが完了しました"
     end
 
     private
+
+    def set_history
+      @history = History.find_by(juge_use: true, user_id: current_user.id)
+    end
 
     def address_entry?
       if History.find_by(juge_use: true,user_id: current_user).nil?
