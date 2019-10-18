d3665a375a397f17f8ec66c97886e456076381f9
関口大地 <>
2019-10-18 20:08:12 +0900

    fix_cart_item_model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,12 +2,13 @@ class CartItem < ApplicationRecord
     belongs_to :cart
 
     def create_history_item!(user_id:, history:)
-      product = Product.find(cart_item.product_id)
-      HistoryItem.create!(product_id: cart_item.product_id,
+      product = Product.find(self.product_id)
+      HistoryItem.create!(product_id: self.product_id,
                           product_price: product.price,
-                          quantity: cart_item.quantity,
+                          quantity: self.quantity,
                           user_id: user_id,
                           history: history
                          )
+                        
     end
 end
